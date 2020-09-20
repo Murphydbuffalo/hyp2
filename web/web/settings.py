4cc311e121e885f73d0aea400e642ef9510db6e5
Dan Murphy <hello@danmurphy.codes>
2020-09-20 16:54:53 -0600

    Configure production environment variables

--- web/web/settings.py
+++ web/web/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_5@i3yzg%99ootbe@r7w8z-o_0pa(1d*ukphx&5pogw27_)ba('
+SECRET_KEY = environ.get('SECRET_KEY', '_5@i3yzg%99ootbe@r7w8z-o_0pa(1d*ukphx&5pogw27_)ba(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = environ.get('DEBUG', 'ON') == 'ON'
 
 ALLOWED_HOSTS = [
     "onhyp.com",
