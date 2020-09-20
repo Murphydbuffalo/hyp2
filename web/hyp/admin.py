b3480f7a34f0d0387b9bcd0ff5283a294cb56850
Dan Murphy <hello@danmurphy.codes>
2020-09-20 12:23:19 -0600

    Include api keys in admin interface

--- web/hyp/admin.py
+++ web/hyp/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from hyp.models import *
 
 admin.site.register(Customer)
+admin.site.register(ApiKey)
 admin.site.register(Experiment)
 admin.site.register(Variant)
 admin.site.register(Interaction)
