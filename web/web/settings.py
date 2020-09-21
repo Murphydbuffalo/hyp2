bd1160f32b72149cad3cd38fa2d5fda151cd40ed
Dan Murphy <hello@danmurphy.codes>
2020-09-20 18:09:17 -0600

    Encrypt cookies in prod

--- web/web/settings.py
+++ web/web/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -32,6 +31,8 @@ AIRBRAKE = dict(
 )
 
 if DEBUG == False:
+    SESSION_COOKIE_SECURE=True
+    CSRF_COOKIE_SECURE=True
     LOGGING = {
         'version': 1,
         'disable_existing_loggers': False,
