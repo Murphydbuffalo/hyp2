ed69d90fba2e40f3ecf2089b515377e739f52efb
Dan Murphy <hello@danmurphy.codes>
2020-11-30 09:07:27 -0700

    Need to handle optional trailing slash in URLs
    Just defined additional routes as a stopgap solution, but may be better
    to handle this with regexeps (via `re_path`). Just want to make sure
    there aren't any performance implications for that.

--- web/hyp/urls.py
+++ web/hyp/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('experiments', views.index, name='index'),
     path('experiments/<int:experiment_id>/', views.show, name='show'),
+    # TODO: URL namespace for api/v1
+    # TODO: maybe use re_path to have a regex handle the trailing slash?
+    # Would Django REST framework take care of this for us?
+    path('variant_assignment/<str:participant_id>/<int:experiment_id>', views.variant_assignment, name='variant_assignment'),
+    path('record_conversion/<str:participant_id>/<int:experiment_id>', views.variant_assignment, name='record_conversion'),
     path('variant_assignment/<str:participant_id>/<int:experiment_id>/', views.variant_assignment, name='variant_assignment'),
     path('record_conversion/<str:participant_id>/<int:experiment_id>/', views.variant_assignment, name='record_conversion'),
 ]
