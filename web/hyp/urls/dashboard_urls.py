be15fb26402d5bad8d82fbbebb050175af65c8f8
Dan Murphy <hello@danmurphy.codes>
2020-12-25 18:33:04 -0700

    Separate views and URLs for landing pages & experiments
    And render a placeholder homepage

--- web/hyp/urls/dashboard_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from hyp.views import dashboard_views
-
-urlpatterns = [
-    path('', dashboard_views.index, name='index'),
-    path('experiments/', dashboard_views.index, name='index'),
-    path('experiments/<int:experiment_id>/', dashboard_views.show, name='show'),
-]
