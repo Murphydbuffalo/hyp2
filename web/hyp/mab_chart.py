a9ed2b5cacff32002e121030eeed7dde989678af
Dan Murphy <hello@danmurphy.codes>
2021-01-14 15:32:09 -0700

    Better formatting for subplot animations

--- web/hyp/mab_chart.py
+++ web/hyp/mab_chart.py
@@ -16,32 +16,44 @@ from matplotlib.animation import FuncAnimation
 # fig.show()
 
 # This works
+x_ticks = np.arange(1000)[::250]
+x = np.arange(1000)[::10]
+y = np.array([[np.sin(point), np.cos(point), point] for point in x])
+
 fig, ax = plt.subplots(1, 3, figsize=(9, 6))
-x, y = [], []
-x_points = np.arange(1000)
-y_points = [np.sin(point) for point in x_points]
+plt.suptitle("Traffic split by variant")
 
-lines = [line.plot(x, y, lw=2) for line in ax]
-
-for axis in ax:
-    axis.set_ylim(0.0, 1.0)
-    axis.set_xlim(0, 1000)
+lines = [line.plot([], [], lw=2) for line in ax]
 
 
 def animate(i):
-    x.append(x_points[i])
-    y.append(y_points[i])
-
     for axis in ax:
         axis.figure.canvas.draw()
 
-    for line in lines:
+    for index, line in enumerate(lines):
         # Only one line per subplot for now
-        line[0].set_data(x, y)
-
-    return lines
+        line[0].set_data(x[0:i + 1], y[0:i + 1, index])
 
 
-animation = FuncAnimation(fig, animate, interval=10)
+def init():
+    for index, axis in enumerate(ax):
+        axis.set_xticks(x_ticks)
+        axis.set_ylim(0.0, 1.0)
+        axis.set_xlim(0, 1010)
+
+        if index == 0:
+            axis.set_ylabel("Traffic percentage")
+
+        if index == 1:
+            axis.set_xlabel("Unique visitors")
+
+
+animation = FuncAnimation(
+    fig,
+    animate,
+    interval=10,
+    repeat=False,
+    init_func=init
+)
 
 plt.show()
