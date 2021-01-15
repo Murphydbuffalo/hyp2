9cd4b8c748a4683e5e1b266998e98cdbc4568c8d
Dan Murphy <hello@danmurphy.codes>
2021-01-15 09:55:03 -0700

    Make things pretty

--- web/hyp/mab_chart.py
+++ web/hyp/mab_chart.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import style, pyplot as plt
 from matplotlib.animation import FuncAnimation
 from hyp.thompson_sampler import ThompsonSampler
 
@@ -31,6 +31,7 @@ x_ticks = np.arange(1000)[::250]
 x = np.arange(1000)[::25]
 y = calculate_traffic_splits([0.05, 0.10, 0.03], x)
 
+style.use("seaborn")
 fig, ax = plt.subplots(1, 3, figsize=(9, 6))
 plt.suptitle("Traffic split by variant")
 
@@ -49,7 +50,7 @@ def animate(i):
 def init():
     for index, axis in enumerate(ax):
         axis.set_xticks(x_ticks)
-        axis.set_ylim(0.0, 1.0)
+        axis.set_ylim(-0.05, 1.05)
         axis.set_xlim(0, 1010)
 
         if index == 0:
@@ -62,7 +63,7 @@ def init():
 animation = FuncAnimation(
     fig,
     animate,
-    interval=667,
+    interval=333,
     repeat=False,
     init_func=init
 )
