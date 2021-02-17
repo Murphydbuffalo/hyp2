6b475279ec1ee5da72d6f545a6e172bca6170e59
Dan Murphy <hello@danmurphy.codes>
2021-02-17 15:59:37 -0700

    Load local env vars with dotenv (.env)

--- web/web/settings.py
+++ web/web/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from os import environ
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
