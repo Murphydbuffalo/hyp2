ba4c715dea91f8f0ceaf6190e53f5215621ef3e9
Dan Murphy <hello@danmurphy.codes>
2021-02-25 15:22:25 -0700

    Don't configure Mailtrap in test environment
    Django automatically sends emails to a dummy outbox during test runs,
    which allows us to make assertions about what mail was sent.

--- web/web/settings/testing.py
+++ web/web/settings/testing.py
@@ -1,5 +1,4 @@
 from .base import * # noqa F403
-from os import environ
 
 DEBUG = False
 
@@ -11,8 +10,3 @@ ALLOWED_HOSTS = [
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_HOST_USER = '2b318f4d13897f'
-EMAIL_HOST_PASSWORD = environ.get('MAILTRAP_PASSWORD')
-EMAIL_PORT = '2525'
