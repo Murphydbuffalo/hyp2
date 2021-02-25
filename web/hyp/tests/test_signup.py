cc544d2e3f523743d2d29c71ec42fc08e877a459
Dan Murphy <hello@danmurphy.codes>
2021-02-25 16:39:05 -0700

    Test confirmation email sent & user created

---
+++ web/hyp/tests/test_signup.py
@@ -0,0 +1,42 @@
+from django.test import TestCase, Client
+from django.core import mail
+from hyp.models import HypUser
+from allauth.account.models import EmailAddress
+
+
+class TestSignup(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_ui_has_form(self):
+        pass
+
+    def test_verification_sends_on_form_submit(self):
+        self.assertEqual(EmailAddress.objects.count(), 0)
+        self.assertEqual(HypUser.objects.count(), 0)
+        self.assertEqual(len(mail.outbox), 0)
+
+        response = self.client.post(
+            '/accounts/signup/',
+            {
+                'email': 'bob@example.com',
+                'password1': 'thisisaverynicepasswordfortesting!',
+                'password2': 'thisisaverynicepasswordfortesting!'
+            },
+            follow=True
+        )
+
+        self.assertEqual(len(response.redirect_chain), 1)
+        self.assertEqual(response.redirect_chain[0], ('/accounts/confirm-email/', 302))
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(HypUser.objects.filter(email='bob@example.com').count(), 1)
+        self.assertEqual(
+            EmailAddress.objects.filter(email='bob@example.com', verified=False).count(),
+            1
+        )
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, '[Hyp] Please Confirm Your E-mail Address')
+
+    def test_email_verifies(self):
+        pass
