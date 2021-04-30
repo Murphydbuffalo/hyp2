3f47f34096e0cd54d5e53c837c2545cdda953551
Dan Murphy <hello@danmurphy.codes>
2021-04-29 18:08:03 -0600

    Check user permissions & 403 when appropriate

--- web/hyp/views/api_key_views.py
+++ web/hyp/views/api_key_views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
@@ -6,27 +7,35 @@ from hyp.models import ApiKey
 
 @login_required
 def index(request):
-    # TODO: check for permission to view API keys
-    api_keys = ApiKey.objects.filter(
-        customer_id=request.user.customer_id
-    ).order_by('-created_at')
+    if request.user.has_perm("hyp.view_apikey"):
+        api_keys = ApiKey.objects.filter(
+            customer_id=request.user.customer_id
+        ).order_by('-created_at')
 
-    return render(request, 'hyp/api_keys/index.html', {"api_keys": api_keys})
+        return render(request, 'hyp/api_keys/index.html', {"api_keys": api_keys})
+    else:
+        raise PermissionDenied
 
 
 @login_required
 def create(request):
-    # TODO: check for permission to create API keys
-    new_key = ApiKey(customer_id=request.user.customer_id, label=request.POST["api_key_label"])
-    new_key.save()
+    if request.user.has_perm("hyp.add_apikey"):
+        new_key = ApiKey(customer_id=request.user.customer_id, label=request.POST["api_key_label"])
+        new_key.save()
 
-    return redirect("/api_keys/")
+        return redirect("/api_keys/")
+    else:
+        raise PermissionDenied
+
+
 
 @login_required
 def deactivate(request, api_key_id):
-    # TODO: check for permission to retire API keys
-    api_key = ApiKey.objects.get(id=api_key_id)
-    api_key.deactivated_at = datetime.now()
-    api_key.save()
+    if request.user.has_perm("hyp.change_apikey"):
+        api_key = ApiKey.objects.get(id=api_key_id)
+        api_key.deactivated_at = datetime.now()
+        api_key.save()
 
-    return redirect("/api_keys/")
+        return redirect("/api_keys/")
+    else:
+        raise PermissionDenied
