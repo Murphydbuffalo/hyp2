008e4ea83f7b01c12df1bb273bc14eb729d41272
Dan Murphy <hello@danmurphy.codes>
2021-05-08 18:49:57 -0600

    Django 3.2 upgrade migration
    Converts primary keys from int to bigint.
    
    Also snuck in a new field for variants: `baseline`, which is a boolean
    denoting if the variant is the customer's current (or "baseline")
    version of the feature they're experimenting with.

--- web/hyp/models.py
+++ web/hyp/models.py
@@ -73,6 +73,9 @@ class Experiment(models.Model):
     def __str__(self):
         return self.name
 
+    def description(self):
+        return f'{self.name}{" (stopped)" if self.stopped else ""}'
+
 
 class VariantManager(models.Manager):
     def for_assignment(self, participant_id, access_token, experiment_id):
@@ -90,6 +93,7 @@ class Variant(models.Model):
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField('created at', auto_now_add=True)
     updated_at = models.DateTimeField('updated at', auto_now=True)
+    baseline = models.BooleanField(default=False)
 
     experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
