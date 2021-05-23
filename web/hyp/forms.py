032a441945808a8492387e8639c4c91a3861457a
Dan Murphy <hello@danmurphy.codes>
2021-05-22 20:25:38 -0600

    Add model forms for experiments and variants
    Django uses these to perform validation and persistence of model
    instances (rather than directly working with the model instance as you
    would in Rails).

--- web/hyp/forms.py
+++ web/hyp/forms.py
@@ -1,16 +1,18 @@
-from hyp.models import HypUser
+from hyp.models import Experiment, HypUser, Variant
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-
+from django.forms import modelform_factory
 
 class HypUserChangeForm(UserChangeForm):
-
     class Meta:
         model = HypUser
         fields = ("username", "email")
 
 
 class HypUserCreationForm(UserCreationForm):
-
     class Meta:
         model = HypUser
         fields = ("username", "email")
+
+
+ExperimentForm = modelform_factory(Experiment, fields=["name", "customer"])
+VariantForm = modelform_factory(Variant, fields=["name", "baseline", "customer", "experiment"])
