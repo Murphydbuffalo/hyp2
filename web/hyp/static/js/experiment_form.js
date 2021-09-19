6f5d5f78d133de366df1f1e0c12307c0792982d7
Dan Murphy <hello@danmurphy.codes>
2021-09-19 17:29:08 -0600

    Style dynamically rendered variant form inputs

--- web/hyp/static/js/experiment_form.js
+++ web/hyp/static/js/experiment_form.js
@@ -5,7 +5,7 @@ document.onreadystatechange = function(_event) {
 
   document.querySelector("#add-variant").onclick = function(event) {
     event.preventDefault();
-    const clonedInput = document.querySelector("#empty-form").cloneNode(true);
+    const clonedInput = document.querySelector("#empty-form input").cloneNode(true);
     const newInputHTML = clonedInput.outerHTML.replace(/__prefix__/g, currentNumForms);
     document.querySelector("#variant-form-fields").insertAdjacentHTML("beforeend", newInputHTML);
     document.querySelector("#id_variant_set-TOTAL_FORMS").value = currentNumForms + 1;
