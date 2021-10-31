d19824481cf361421986eb09c983f9646036a6c4
Dan Murphy <hello@danmurphy.codes>
2021-10-31 14:17:27 -0600

    Only schedule variant metric jobs once per day

--- web/hyp/apps.py
+++ web/hyp/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from datetime import datetime
 
 
 class HypConfig(AppConfig):
@@ -7,6 +8,7 @@ class HypConfig(AppConfig):
     def ready(self):
         from hyp import signals # noqa F401
         from hyp.jobs.variant_metrics import enqueue_all
+        from hyp.models import IdempotencyKey
 
         import django_rq
         import logging
@@ -17,9 +19,18 @@ class HypConfig(AppConfig):
 
         logger.info('App starting up, calling HypConfig.ready()')
 
-        # Runs every day at 2:00AM Mountain / 8:00AM UTC
-        # https://crontab.guru/every-day-at-2am
-        scheduler.cron(
-            "0 8 * * *",
-            func=enqueue_all,
-        )
+        idempotency_key = f'enqueue_variant_metrics_{datetime.now().date()}'
+
+        if IdempotencyKey.objects.filter(key=idempotency_key).exists():
+            return
+        else:
+            IdempotencyKey(key=idempotency_key).save()
+
+            # Runs every day at 8:00AM UTC.
+            # So for US time zones this will run at midnight/1:00AM/2:00AM/3:00AM or
+            # 1:00AM/2:00AM/3:00AM/4:00AM depending on daylight savings time.
+            # https://crontab.guru/every-day-at-2am
+            scheduler.cron(
+                "0 8 * * *",
+                func=enqueue_all,
+            )
