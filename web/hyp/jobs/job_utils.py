921940a0fa6be4cb54919ba80d7d642d88e9c39b
Dan Murphy <hello@danmurphy.codes>
2021-11-07 09:18:10 -0700

    `scheduler` management command to de-dupe & schedule periodic jobs

--- web/hyp/jobs/job_utils.py
+++ web/hyp/jobs/job_utils.py
@@ -1,5 +1,18 @@
+from hyp.models import IdempotencyKey
 from rq import Retry
+
+import django_rq
 
 
 def retries(max=5, interval=[4, 16, 64, 256, 1028]):
      return Retry(max=max, interval=interval)
+
+def clear_scheduled_jobs():
+     for job in get_scheduled_jobs():
+          job.delete()
+
+def get_scheduled_jobs():
+     return get_scheduler().get_jobs()
+
+def get_scheduler():
+     return django_rq.get_scheduler()
