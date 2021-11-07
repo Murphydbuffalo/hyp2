7de897c4b8c17bdcf53355f781eb691c8d03f4d3
Dan Murphy <hello@danmurphy.codes>
2021-11-07 11:36:18 -0700

    Convenience function for idempotently enqueuing jobs
    And wrapping `django_rq` so that dependency isn't sprinkled throughout
    the code

--- web/hyp/jobs/sample_data.py
+++ web/hyp/jobs/sample_data.py
@@ -1,10 +1,6 @@
-from hyp.jobs.job_utils import retries
+from hyp.jobs.job_utils import enqueue
 from hyp.data.generators import generate_sample_data
 
-import django_rq, logging
-
-logger = logging.getLogger(__name__)
 
 def enqueue(experiment):
-    logger.info(f'Enqueuing experiment sample data job for {experiment.id}')
-    django_rq.enqueue(generate_sample_data, experiment, retry=retries())
+    enqueue(func=lambda: generate_sample_data(experiment))
