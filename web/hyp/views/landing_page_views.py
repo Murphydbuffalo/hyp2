814169f7746af29575cc1a47395431d0a7502bb7
Dan Murphy <hello@danmurphy.codes>
2022-03-24 15:57:36 -0600

    Don't require authorization for landing pages
    And redirect to the landing page rather than sign in if user isn't
    authenticated.

--- web/hyp/views/landing_page_views.py
+++ web/hyp/views/landing_page_views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-@login_required
 def index(request):
     return render(request, 'hyp/landing_pages/home.html', {})
