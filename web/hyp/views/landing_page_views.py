9d2cc9d8a1746251c762307e887a6f4e97eb875f
Dan Murphy <hello@danmurphy.codes>
2022-03-27 18:48:25 -0600

    Use hyp client to get variant assignment

--- web/hyp/views/landing_page_views.py
+++ web/hyp/views/landing_page_views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from os import environ
+from hyp_client.v1 import HypClient
 
 def index(request):
-    return render(request, 'hyp/landing_pages/home.html', {})
+    hyp = HypClient(environ.get('HYP_API_TOKEN'))
+    user_id = 1
+    experiment_id = 12 # landing page graphic experiment
+    response = hyp.assignment(participant_id=user_id, experiment_id=experiment_id)
+    variant = "Animated screenshot"
+
+    if response["message"] == "success":
+        variant = response["payload"]["variant_name"]
+
+    return render(request, 'hyp/landing_pages/home.html', { "variant": variant })
